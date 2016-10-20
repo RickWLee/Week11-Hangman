447311c69580c5747f3a47d9a7403ad6d64ef8ec
Weihao Rick Lee <kenchi133@hotmail.com>
2016-10-19 22:04:58 -0500

    create array to mask the selected random word/letters

--- word.js
+++ word.js
@@ -6,11 +6,16 @@ var word = require('./game.js');
 
 //set the array for the PCselectedLetter
 var PCselectedLetter = [];
+var maskLetter=[];
 
 
 for (i=0; i<word.length; i++){
+	//take the selected random word and put it into each letter in the array PCselectedLetter
 	PCselectedLetter[i]=word.charAt(i);
+	//create "_" to mask the correct letters for the PCselectedLetter
+	maskLetter[i]='_';
 }
 
 
 console.log(PCselectedLetter);
+console.log(maskLetter);
